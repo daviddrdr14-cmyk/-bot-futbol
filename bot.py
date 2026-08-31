68aa2658417b4e12ab7483bec0ee7afb1053963f
daviddrdr14-cmyk <david.dr.dr14@gmail.com>
2026-08-30 23:48:31 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,17 @@
-import os,re,time,requests,telebot,unicodedata
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Bot de futbol funcionando!"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+Thread(target=run_flask).start()import os,re,time,requests,telebot,unicodedata
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN=os.getenv("BOT_TOKEN")
